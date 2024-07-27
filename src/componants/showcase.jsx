c746870addd44edaec5e3a6788c0dcdaec29a4c1
Your GitHub Username <ajay.poddar15fea@gmail.com>
2024-07-27 23:40:14 +0530

    made it responsive

--- src/componants/showcase.jsx
+++ src/componants/showcase.jsx
@@ -1,17 +1,16 @@
 const Showcase = () => {
   return (
-    <div className="main container mx-auto ">
-      <div className="text-gray-300    md:text-6xl   w-screen h-full">
-        <h2 className="text-pop-up-right">
+    <div className="main container mx-auto px-10 ">
+      <div className="text-gray-300   px-2  md:text-6xl   w-full h-full">
+        <h2 className="text-pop-up-right text-2xl md:text-4xl lg:text-6xl ">
           A17 Pro chip. <br /> A monster win for gaming.
         </h2>
-        <span>
-          <p className="md:text-3xl mt-8 text-gray-600">
-            It’s here. The biggest redesign in the history of Apple GPUs.
-          </p>
-        </span>
+
+        <p className="text-lg  first-line: py-2 md:text-2xl  mt-2 md:mt-4 text-gray-600">
+          It’s here. The biggest redesign in the history of Apple GPUs.
+        </p>
       </div>
-      <div className="videosection md:py-10   h-2/3">
+      <div className="videosection md:mt-10     border-2 border-gray-800">
         <video
           className="w-full h-full object-cover"
           playsInline
