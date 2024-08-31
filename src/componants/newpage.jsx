3f3799eaf95528285eab0ceab3e0a0472e501207
Your GitHub Username <ajay.poddar15fea@gmail.com>
2024-08-31 23:47:11 +0530

    added the explore section

---
+++ src/componants/newpage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const newpage = () => {
+  return (
+    <div>
+      <h1>this is new page setup for react learning tailwind css </h1>
+    </div>
+  );
+};
+
+export default newpage;
